0c0d2387aea2ca64c53819e52c565a2dfdf45b3b
F4Tornado school compy <h.rovnyak@gmail.com>
2021-05-02 21:00:17 -0400

    Vacation days (warning: janky)

--- sketch.js
+++ sketch.js
@@ -1,16 +1,25 @@
+const toUpdate = JSON.parse(localStorage.getItem("data")) || {
+    date: Date.now(),
+    start: Date.now() + 1000 * 60 * 60 * 24,
+    classes: [{
+        name: "Name",
+        done: 0,
+        total: 1,
+        todo: 0,
+        endDate: new Date().toString()
+    }],
+    vacations: []
+};
+
+if (toUpdate.vacations == undefined) {
+    toUpdate.vacations = [];
+}
+
+localStorage.setItem("data", JSON.stringify(toUpdate));
+
 const vm = new Vue({
     el: "#app",
-    data: JSON.parse(localStorage.getItem("data")) || {
-        date: Date.now(),
-        start: Date.now()+1000*60*60*24,
-        classes: [{
-            name: "Name",
-            done: 0,
-            total: 1,
-            todo: 0,
-            endDate: new Date().toString()
-        }]
-    },
+    data: JSON.parse(localStorage.getItem("data")),
     computed: {
         totalDone: function () {
             return this.classes.reduce((acc, v) => {
@@ -35,8 +44,8 @@ const vm = new Vue({
         percentage: function (Class) {
             return Math.round(Class.done / Class.total * 10000) / 100;
         },
-        percentageAndTodo: function(Class) {
-            return (Class.done+Class.todo)/Class.total;
+        percentageAndTodo: function (Class) {
+            return (Class.done + Class.todo) / Class.total;
         },
         addNew: function () {
             this.classes.push({
@@ -46,29 +55,35 @@ const vm = new Vue({
                 todo: 0
             });
         },
+        newVacation: function () {
+            this.vacations.push(new Date().toString());
+        },
         remove: function (i) {
             this.classes.splice(i, 1);
         },
+        removeVacation: function (i) {
+            this.vacations.splice(i, 1);
+        },
         todo: function () {
             for (let i = 0; i < this.classes.length; i++) {
-                this.classes[i].todo = Math.round(100 * (this.classes[i].total - this.classes[i].done) / days(new Date(), new Date(this.classes[i].endDate))) / 100;
+                this.classes[i].todo = Math.round(100 * (this.classes[i].total - this.classes[i].done) / days(new Date(), new Date(this.classes[i].endDate), this.vacations)) / 100;
             }
         }
     },
     updated: function () {
         localStorage.setItem("data", JSON.stringify({
-           classes: this.classes,
+            classes: this.classes,
         }));
     }
 });
 
 // https://stackoverflow.com/questions/37069186/calculate-working-days-between-two-dates-in-javascript-excepts-holidays
-function days(startDate, endDate) {
+function days(startDate, endDate, vacations) {
     var count = 0;
     var curDate = startDate;
     while (curDate <= endDate) {
         var dayOfWeek = curDate.getDay();
-        if (!((dayOfWeek == 6) || (dayOfWeek == 0)))
+        if (!((dayOfWeek == 6) || (dayOfWeek == 0) || vacations.map(x => { let d = new Date(x); d.setDate(d.getDate() + 1); return d.toDateString(); }).includes(curDate.toDateString())))
             count++;
         curDate.setDate(curDate.getDate() + 1);
     }
